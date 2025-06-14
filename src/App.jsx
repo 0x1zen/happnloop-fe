42929627dba2f1717fd62fe3a3e743c0b667d029
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2025-06-14 10:37:14 +0000

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import Navbar from "./components/Navbar";
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Body from "./components/Body.jsx";
+import LoginPage from "./components/LoginPage.jsx";
+import ProfilePage from "./components/ProfilePage.jsx";
 export default function App() {
   return (
     <div>
-      <Navbar></Navbar>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
