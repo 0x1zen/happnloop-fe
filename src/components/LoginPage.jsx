e729d9f2f29d324aa0ee70a29ca787c60049c6a8
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2025-06-14 14:27:16 +0000

    axios and cookies

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,9 +1,65 @@
-const LoginPage = () =>{
-  return(
+import axios from "axios";
+import { useRef } from "react";
+
+const LoginPage = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const handleLogin = async () => {
+    try{
+      const emailId = emailRef.current.value;
+      const password = passwordRef.current.value;
+      const response = await axios.post(
+        "https://4799d8a9-fea0-423a-819e-aa6ae6edd0e9-00-3f0wju7f2j896.sisko.replit.dev/login",
+        {
+          emailId,
+          password,
+        },
+        {withCredentials:true}
+      );
+      console.log(response.data);
+    }catch(e){
+      console.log("Error"+ e.message)
+    }
+    
+  };
+  return (
     <div>
-    Login Page
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(https://images.alphacoders.com/829/829094.jpg",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center w-full">
+          <div className="max-w-lg w-full space-y-6">
+            <h1 className="mb-5 text-6xl font-bold">Login</h1>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend text-2xl">Email</legend>
+              <input
+                ref={emailRef}
+                type="email"
+                className="input input-lg w-full"
+                placeholder="Type here"
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend text-2xl ">Password</legend>
+              <input
+                ref={passwordRef}
+                type="password"
+                className="input input-lg w-full"
+                placeholder="Type here"
+              />
+            </fieldset>
+            <button className="btn btn-secondary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginPage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,18 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">happnloop</a>
+        <a className="btn btn-ghost text-4xl mx-5">
+          happnloop<p className="text-secondary">.</p>
+        </a>
       </div>
       <div className="flex gap-2">
-        <div className="dropdown dropdown-end mx-5">
+        <div className="dropdown dropdown-end mx-7">
           <div
             tabIndex={0}
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
-            <div className="w-10 rounded-full">
+            <div className="w-25 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
